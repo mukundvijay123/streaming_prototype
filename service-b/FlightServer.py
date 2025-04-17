39841b32fe6d4d67b43cd640bd2523ec3a06eaec
Varun S <158815814+Varun-S5@users.noreply.github.com>
2025-04-17 15:48:32 +0530

    Shared Memory in service-b.py FlightServer.py

--- service-b/FlightServer.py
+++ service-b/FlightServer.py
@@ -3,9 +3,6 @@ import pyarrow.flight as flight
 import multiprocessing.shared_memory
 from datetime import datetime
 from SharedMemoryResources import SharedMemoryResources
-BUFFER_SIZE = 1000  # Max number of messages
-HEADER_SIZE = 16  # 8 bytes for size, 8 bytes for offset
-DATA_SECTION_SIZE = 2048 * 18  # 100MB for data section
 #0                   16 * 10K, 
 #[HEADER1, HEADER2, ... 10000, DATA1, DATA2, ...]
 # R, W                           R_D_I, W_D_I
@@ -13,7 +10,7 @@ DATA_SECTION_SIZE = 2048 * 18  # 100MB for data section
 #[OFFSET -> 8 BYTES]
 class FlightServer(flight.FlightServerBase):
     def __init__(self, shared_memory_name, lock, write_index, read_index, data_section_start, 
-                write_data_idx, read_data_idx, location, event, event2):
+                write_data_idx, read_data_idx, location, event, event2,header_size,buffer_size):
         super().__init__(location)
         self.shm_name = shared_memory_name
         self.event = event
@@ -26,18 +23,12 @@ class FlightServer(flight.FlightServerBase):
         self.shm = multiprocessing.shared_memory.SharedMemory(name=shared_memory_name)
         self.data_section_size = self.shm.size - self.data_section_start.value
         self.event2 = event2
+        self.header_size = header_size
+        self.buffer_size = buffer_size
     def do_put(self, context, descriptor, reader, flight_writer):
         try:
-            table = reader.read_chunk()
-            if not table:
-                return
+            table = reader.read_all()
             
-            record_batch = table.data
-            schema_metadata = record_batch.schema
-
-
-           # new_schema = record_batch.schema.with_metadata(schema_metadata)
-            table = pa.Table.from_batches([record_batch], schema=schema_metadata)
             # Use the SharedMemoryResources class to write data
             shared_memory = SharedMemoryResources(
                 multiprocessing.shared_memory.SharedMemory(name=self.shm_name),#THIS IS THR NAME
@@ -48,7 +39,9 @@ class FlightServer(flight.FlightServerBase):
                 self.write_data_idx,
                 self.read_data_idx,
                 self.event,
-                self.event2
+                self.event2,
+                self.header_size,
+                self.buffer_size
             )
             
             # Write the table to shared memory
