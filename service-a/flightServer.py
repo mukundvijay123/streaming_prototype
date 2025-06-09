70948fb98e64ea4dd07166095a6d43f828cf6cfc
mukundvijay123 <vijay.mukund7845@gmail.com>
2025-06-09 18:00:57 +0530

    added schema fuctionality to service-a not tested

--- service-a/flightServer.py
+++ service-a/flightServer.py
@@ -2,13 +2,38 @@ import pyarrow as pa
 import pyarrow.flight as flight
 from utils import is_valid_grpc_address,extract_subscription
 from metadata import systemMetadata
+import adbc_driver_postgresql.dbapi as adbc
 
+
+DB_URI="postgresql://postgres:123456789@localhost:5432/arrow_kafka"
 
 class FlightServer(flight.FlightServerBase):
     def __init__(self,systemMetadata:systemMetadata,location="grpc://0.0.0.0:8815"):
         super().__init__(location)
         self._location=location
         self.systemMetadata=systemMetadata
+        self.adbcConn=adbc.connect(uri=DB_URI)
+
+    def __GetStreamSchema(self,topic):
+        cursor=self.adbcConn.cursor()
+        query=("SELECT * FROM stock_prices_2 WHERE stock_symbol = $1;")
+        cursor.execute(query,(topic,))
+        data=cursor.fetch_arrow_table()
+        return data.schema
+
+    def get_schema(self, context, descriptor):
+        # Expecting the topic to be passed as a path descriptor
+        if descriptor.descriptor_type == flight.DescriptorType.PATH:
+            # Get topic from path
+            topic = descriptor.path[0]
+            
+            # Get the Arrow Schema from the source (e.g., PostgreSQL via ADBC)
+            schema = self.__GetStreamSchema(topic)
+            
+            # Return as a SchemaResult
+            return flight.SchemaResult(schema)
+        
+        raise flight.FlightServerError("Unsupported descriptor type or missing topic")
 
     
     def list_actions(self,context):
