0fe7ff3cb7697f518f0867a086f565a0cf17d6f9
mukundvijay123 <vijay.mukund7845@gmail.com>
2025-06-09 11:06:50 +0530

    moving flight Demo main class to  misc

--- service-b-stateful/app/src/main/java/serviceb/misc/DemoMain.java
+++ service-b-stateful/app/src/main/java/serviceb/misc/DemoMain.java
@@ -1,4 +1,4 @@
-package serviceb.flightServer;
+package serviceb.misc;
 
 import java.time.LocalDateTime;
 import java.util.Arrays;
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.*;
 
+import serviceb.flightServer.ServiceBFlightConsumer;
 import serviceb.flightServer.ChDequeue.ChQDequeueWorker;
 import serviceb.flightServer.ChDequeue.QueueMessage;
 
